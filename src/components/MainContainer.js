c3f1217d24ff1b56c3c1dd2ed3f7b176f57c63b4
Rahul P S <rahulps995@gmail.com>
2023-09-11 21:52:58 +0530

    video title build copleted

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -7,11 +7,12 @@ const MainContainer = () => {
     const movies=useSelector(store=>store.movies?.nowPlayingMovies)
     if(movies==null)return //if movies is not present then return  or it cause error
     const mainMovie=movies[0]
-    console.log(mainMovie)
+  
+    const {original_title,overview}=mainMovie
   return (
     <div>
         <VideoBackground/>
-        <VideoTitle movie={mainMovie}/>
+        <VideoTitle title={original_title} overview={overview}/>
     </div>
   )
 }
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const VideoTitle = () => {
+const VideoTitle = ({title,overview}) => {
+    
   return (
-    <div>VideoTitle</div>
+    <div className='pt-36 px-12'>
+        <h1 className='text-6xl font-bold'>{title}</h1>
+        <p className='py-6 text-lg w-1/3 '>{overview}</p>
+        <div>
+            <button className=' bg-gray-500 rounded-lg px-12 text-lg  p-2   text-white bg-opacity-50'>▷ Play</button>
+            <button className='mx-3 bg-gray-500 rounded-lg px-12 text-lg  p-2   text-white bg-opacity-50'>More Info</button>
+        </div>
+    </div>
   )
 }
 
